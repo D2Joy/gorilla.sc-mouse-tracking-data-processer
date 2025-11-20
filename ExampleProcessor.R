b8876d8b30f4fc85b167b3a11e4615c8df8b68e9
D2Joy <61085405+D2Joy@users.noreply.github.com>
2025-11-20 16:44:05 +0000

    Update ExampleProcessor.R
    add example code to read behavioural data and spreadsheet.

--- ExampleProcessor.R
+++ ExampleProcessor.R
@@ -1,5 +1,30 @@
 ###example code with parallel processing
-filepaths <- list.files(#read trajectory data file path
+# read behavioural data from the source folder & create a list of all the file paths
+filepaths_base <- list.files(#basic data files
+  getwd(),
+  pattern = ".*_task-.*.csv",
+  full.names = TRUE
+)
+
+baseT<-foreach(i = 1 :length(filepaths_base),.combine=rbind,.verbose = F)%do%{#
+  filepath<-filepaths_base[i]
+  # status message (handy for debugging)
+  cat("processing", filepath, "...","\n")
+  
+  current_data <-fread(filepath,fill = F,verbose = F, check.names=T,na.strings="NA")#,header=T
+  #current_data <-read.csv(filepath)
+  
+  #score incorrect as -1
+  current_data[Attempt==1 &Incorrect==1]$Correct<--1
+  current_data[Response==""]$Response<-NA
+  
+  return(current_data)
+}
+
+sst<-fread(paste(getwd(), "/spreadsheet.csv", sep = ""))
+
+#read trajectory data file paths into a list
+filepaths <- list.files(
   paste0(getwd(),"/uploads"),
   pattern = "*.xlsx",
   full.names = TRUE
@@ -19,3 +44,4 @@ system.time(merged_slice<-foreach (i = 1:length(filepaths),.combine=rbind,.verbo
                                      
                                      return(sliced_coor)
                                    })
+
